a3cb4aa5af5b7e3ba604b366ebb3918ccf89a8e1
Chandan Kumar <chandan765462@gmail.com>
2026-07-31 17:07:34 +0530

    Add Java programs

---
+++ Array_Function/Function_Method.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Function_Method{
+  public static void main(String[] args) {
+    int first = readNumber();
+    int second = readNumber();
+    int sum = first + second;
+    System.out.print("Total Sum: " + sum);
+  }
+  public static int readNumber(){
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter your number: ");
+    int number = input.nextInt();
+    return number;
+  }
+}
